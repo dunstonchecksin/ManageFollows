24fdb38b2ec409e6c6a03c013c9fdeed8d9a99f2
Aaron Nozaki <aaronn321@gmail.com>
2016-09-17 12:46:09 -0700

    init

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var accessToken = location.href.split('access_token=')[1];
 console.log(accessToken);
-$.ajax({url: "https://api.instagram.com/v1/users/self/follows?access_token="+accessToken, success: function(result){
+$.ajax({url: "https://api.instagram.com/v1/users/self/follows?access_token="+accessToken+'&callback=?', success: function(result){
         console.log(result);
     }});
+$.getJSON(
+        'https://api.instagram.com/v1/users/25025320/media/recent/?client_id=YOUR CLIENT ID&callback=?',
+        function(data) {
+            console.log(data);
+        }
+    );
