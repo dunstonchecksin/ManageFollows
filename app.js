ecc70eeb95d2a42535636edb6516d802a48700aa
Aaron Nozaki <aaronn321@gmail.com>
2016-09-17 12:40:47 -0700

    init

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var accessToken = location.href.split('access_token=')[1];
 console.log(accessToken);
+$.ajax({url: "https://api.instagram.com/v1/users/self/follows?access_token="+accessToken, success: function(result){
+        console.log(result);
+    }});
